6d8842ee63125b3c04cf385cc3230387aba45ffe
freddy <fredsong99@gmail.com>
2023-04-29 15:42:45 -0700

    Begin user input for testing the model.

--- src/predict.py
+++ src/predict.py
@@ -6,9 +6,6 @@ model = load_model('model.h5')
 # generate predictions on test data
 predictions = model.predict(X_test)
 
-# convert predicted labels from one-hot encoding to integers
-#predictions_int = np.argmax(predictions, axis=1)
-
 # compare predicted labels with true labels
 accuracy = np.mean(predictions == y_test)
 
