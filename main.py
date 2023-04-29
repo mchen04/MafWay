8ef1ca8bb0db6532013a089de57ea3394be5fa28
mchen04 <michaelluochen1@gmail.com>
2023-04-29 10:32:44 -0700

    requirements.txt

---
+++ main.py
@@ -0,0 +1,32 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+# set up a Sequential model
+model = Sequential()
+
+# add a convolutional layer with 32 filters, a 3x3 kernel, and ReLU activation
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(img_width, img_height, 3)))
+
+# add a max pooling layer
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# add another convolutional layer with 64 filters
+model.add(Conv2D(64, (3, 3), activation='relu'))
+
+# add another max pooling layer
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# add a flattening layer
+model.add(Flatten())
+
+# add a dense layer with 128 neurons and ReLU activation
+model.add(Dense(128, activation='relu'))
+
+# add an output layer with one neuron for each class and softmax activation
+model.add(Dense(82, activation='softmax'))
+
+# compile the model with appropriate loss function, optimizer, and metrics
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# train the model on your data
+model.fit(train_data, train_labels, validation_data=(test_data, test_labels), epochs=10, batch_size=32)
