ae3265dc26de59d70ee33baf3fd2bfe73cb4086c
Nolancchu <103982110+Nolancchu@users.noreply.github.com>
2023-04-29 22:35:22 -0700

    added uploading to cloudinary functionality

--- projectsite/components/MainGraphic.jsx
+++ projectsite/components/MainGraphic.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const MainGraphic = () => {
+    const [imageSrc, setImageSrc] = useState();
+    const [uploadData, setUploadData] = useState();
+
+    function handleOnChange(changeEvent) {
+        const reader = new FileReader();
+    
+        reader.onload = function(onLoadEvent) {
+          setImageSrc(onLoadEvent.target.result);
+          setUploadData(undefined);
+        }
+    
+        reader.readAsDataURL(changeEvent.target.files[0]);
+    }
+
+    async function handleOnSubmit(event) {
+        event.preventDefault();
+    
+        const form = event.currentTarget;
+        const fileInput = Array.from(form.elements).find(({ name }) => name === 'file');
+    
+        const formData = new FormData();
+    
+        for ( const file of fileInput.files ) {
+          formData.append('file', file);
+        }
+    
+        formData.append('upload_preset', 'my-uploads');
+    
+        const data = await fetch('https://api.cloudinary.com/v1_1/dp0bizotu/image/upload', {
+          method: 'POST',
+          body: formData
+        }).then(r => r.json());
+    
+        setImageSrc(data.secure_url);
+        setUploadData(data);
+    }
 
     return (
         <div className="w-full h-full flex flex-row items-center">
@@ -14,14 +51,24 @@ const MainGraphic = () => {
                 whileHover={{ scale: 1.15, }}
                 href={"https://legacy.reactjs.org/docs/getting-started.html"}
                 >
-                    <div class="flex items-center justify-center">
-                        <label for="dropzone-file" class="flex flex-col items-center rounded-3xl justify-center w-96 h-40 border-[3px] border-ncc-beige cursor-pointer bg-ncc-brown">
-                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                                <svg aria-hidden="true" class="w-10 h-10 mb-3 text-ncc-beige" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                                <p class="mb-2 text-sm text-ncc-beige"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                                <p class="text-xs text-ncc-beige">SVG | PNG | JPG</p>
+                    <div className="flex items-center justify-center text-ncc-beige">
+                        <label htmlFor="dropzone-file" className="flex flex-col items-center rounded-3xl justify-center w-96 h-40 border-[3px] border-ncc-beige cursor-pointer bg-ncc-brown">
+                            <div className="flex flex-col items-center justify-center pt-5 pb-6 ml-16">
+                                
+                                <form method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
+                                <p>
+                                    <input type="file" name="file" />
+                                </p>  
+                                
+                                {imageSrc && !uploadData && (
+                                    <p>
+                                    <button>Upload Files</button>
+                                    </p>
+                                )}
+
+                            </form>
                             </div>
-                            <input id="dropzone-file" type="file" class="hidden" />
+
                         </label>
                     </div> 
                 </motion.a>
