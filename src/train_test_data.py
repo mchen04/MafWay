f1dc8175bfdafdbde458e6398cacc2f6dc70bf50
Michael Chen <michaelluochen1@gmail.com>
2023-05-02 14:56:37 -0700

    updated no double code

--- src/train_test_data.py
+++ src/train_test_data.py
@@ -77,92 +77,11 @@ model = tf.keras.Sequential([
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Define the callback
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, min_lr=0.00001)
-
-import numpy as np
-import tensorflow as tf
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
-from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ReduceLROnPlateau
-import matplotlib.pyplot as plt
-
-num_classes = 82
-
-# assume X and y are your data and labels
-X = np.load('images_array.npy')
-y = np.load('labels_array.npy')
-
-datagen = ImageDataGenerator(
-    rotation_range=30, # rotate the image by up to 30 degrees
-    width_shift_range=0.1, # shift the image horizontally by up to 10% of the width
-    height_shift_range=0.1, # shift the image vertically by up to 10% of the height
-    shear_range=0.1, # apply shearing transformation with a maximum of 10% shear
-    zoom_range=0.1, # zoom in/out of the image by up to 10%
-)
-
-datagen.fit(X.reshape((-1, 45, 45, 1)))
-
-# one-hot encode the labels
-y = to_categorical(y, num_classes)
-
-# shuffle the data
-num_samples = len(X)
-indices = np.arange(num_samples)
-np.random.seed(42)
-np.random.shuffle(indices)
-
-X = X[indices]
-y = y[indices]
-
-# split the data into training, validation, and testing sets
-num_train_samples = int(0.6*num_samples)
-num_val_samples = int(0.2*num_samples)
-num_test_samples = num_samples - num_train_samples - num_val_samples
-
-X_train = X[:num_train_samples]
-y_train = y[:num_train_samples]
-
-X_val = X[num_train_samples:num_train_samples+num_val_samples]
-y_val = y[num_train_samples:num_train_samples+num_val_samples]
-
-X_test = X[num_train_samples+num_val_samples:]
-y_test = y[num_train_samples+num_val_samples:]
-
-# build and compile the model
-model = tf.keras.Sequential([
-    Conv2D(filters=32, kernel_size=(3,3), activation='relu', input_shape=(45,45,1)),
-    BatchNormalization(),
-    Conv2D(filters=32, kernel_size=(3,3), activation='relu'),
-    MaxPooling2D(pool_size=(2,2)),
-    Dropout(0.25),
-    Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
-    BatchNormalization(),
-    Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
-    MaxPooling2D(pool_size=(2,2)),
-    Dropout(0.25),
-    Conv2D(filters=128, kernel_size=(3,3), activation='relu'),
-    BatchNormalization(),
-    Conv2D(filters=128, kernel_size=(3,3), activation='relu'),
-    MaxPooling2D(pool_size=(2,2)),
-    Dropout(0.25),
-    Flatten(),
-    Dense(512, activation='relu'),
-    Dropout(0.5),
-    Dense(256, activation='relu'),
-    Dropout(0.5),
-    Dense(num_classes, activation='softmax')
-])
-
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
-
-# Define the callback
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=2, min_lr=0.00001)
 
 # Train the model
-history = model.fit(datagen.flow(X_train.reshape((-1, 45, 45, 1)), y_train, batch_size=32),
-                    epochs=20,
+history = model.fit(datagen.flow(X_train.reshape((-1, 45, 45, 1)), y_train, batch_size=120),
+                    epochs=35,
                     validation_data=(X_val.reshape((-1, 45, 45, 1)), y_val),
                     callbacks=[reduce_lr])
 
